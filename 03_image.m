5e1ba9b48f550c8b5eb39be3ebcc1c50e25899da
24Savagez <73163550+24Savagez@users.noreply.github.com>
2020-10-31 02:21:49 +0700

    03

--- 03_image.m
+++ 03_image.m
@@ -6,7 +6,6 @@ clc
 #Matrix
 img = imread('peppers_Bluehills.png');
 figure(1)
-
 #show the image
 imshow(img)
 
@@ -17,27 +16,27 @@ varsize = size(img)
 temp = img(1,1,1)
 #1:red 2:green 3:blue
 
+#RGB colorspace to CMYK
 #create K,C,M and Y
-K = zeros(varsize(1),varsize(2));
+K =zeros(varsize(1),varsize(2));
 
-C = zeros(varsize(1),varsize(2));
+C =zeros(varsize(1),varsize(2));
 
-M = zeros(varsize(1),varsize(2));
+M =zeros(varsize(1),varsize(2));
 
-Y = zeros(varsize(1),varsize(2));
+Y =zeros(varsize(1),varsize(2));
 
 #max
 #max(img(i,j,:)));
 #max(img(i,j,1),img(i,j,2),img(i,j,3));
 
-#K
+
 for i = 1:varsize(1)
   for j = 1:varsize(2)
-    K(i,j) = (2^16)-(max(img(i,j,:)));
-    C(i,j) = (2^16)-(img(i,j,1))-(img(i,j,2))/K(i,j);
-    M(i,j) = (2^16)-(img(i,j,2))- K(i,j)/(2^16)-K(i,j);
-    Y(i,j) = (2^16)-(img(i,j,3))- K(i,j)/(2^16)-K(i,j);
-    
+    K(i,j) = (2^16)-(max(img(i,j,:)));  
+    C(i,j) = (2^16)-(img(i,j,1))-((2^16)-(max(img(i,j,:))))/(2^16)-(max(img(i,j,:)));
+    M(i,j) = (2^16)-(img(i,j,2))-((2^16)-(max(img(i,j,:))))/(2^16)-(max(img(i,j,:)));
+    Y(i,j) = (2^16)-(img(i,j,3))-((2^16)-(max(img(i,j,:))))/(2^16)-(max(img(i,j,:)));
   endfor
 endfor
 K = uint16(K);
