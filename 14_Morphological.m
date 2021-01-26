c4905f480b6247cb78f16649eec0012f6ffac178
Chutayu Adhitayawong <73163550+24Savagez@users.noreply.github.com>
2021-01-27 05:16:28 +0700

    ...

---
+++ 14_Morphological.m
@@ -0,0 +1,21 @@
+clear all,clc
+
+[file,fpath] = uigetfile({"*.jpg;*.png","choose the image"});
+
+img = imread([fpath,file]);
+VThreshold = 100;
+
+final_img = Binarizations(img,VThreshold);
+
+se_disk = strel("disk", 5, 0);
+se_diamond = strel("diamond", 5);
+
+imOut1 = imclose(img, se_disk);
+imOut2 = imnoise(img, "gaussian");
+
+figure(1)
+imshow(imOut1);
+
+figure(2)
+imshow(imOut2);
+
